2d6fe8b662740f76e1290232bb1cda350e513a1a
alex-snell <audrey.snell@outlook.com>
2025-10-08 11:55:48 -0500

    todos

--- app/src/main/java/edu/luc/etl/cs313/android/shapes/model/BoundingBox.java
+++ app/src/main/java/edu/luc/etl/cs313/android/shapes/model/BoundingBox.java
@@ -1,5 +1,7 @@
 package edu.luc.etl.cs313.android.shapes.model;
 
+import java.util.List;
+
 /**
  * A shape visitor for calculating the bounding box, that is, the smallest
  * rectangle containing the shape. The resulting bounding box is returned as a
@@ -17,24 +19,49 @@ public class BoundingBox implements Visitor<Location> {
 
     @Override
     public Location onFill(final Fill f) {
-        return null;
+        return new Location(0,0,f);
     }
 
     @Override
     public Location onGroup(final Group g) {
+        int minX = 0;
+        int minY = 0;
+        int maxX = 0;
+        int maxY = 0;
+        var shapes = g.getShapes();
+        for(int i = 0; i < shapes.size(); i++){
+           Location boundingBox = shapes.get(i).accept(this);
 
-        return null;
+           int left = boundingBox.getX();
+           int top = boundingBox.getY();
+           Rectangle rect = (Rectangle) boundingBox.getShape();
+           int right = left + rect.getWidth();
+           int bottom = top + rect.getHeight();
+
+           minX = Math.min(minX, left);
+           minY = Math.min(minY, top);
+           maxX = Math.max(maxX, right);
+           maxY = Math.max(maxY, bottom);
+        }
+        int width = maxX- minX;
+        int height = maxY - minY;
+        return new Location(minX,minY,new Rectangle(width,height));
     }
 
     @Override
     public Location onLocation(final Location l) {
-
-        return null;
+        Location innerBox = l.accept(this);
+        int innerX = innerBox.getX();
+        int innerY = innerBox.getY();
+        Rectangle rect = (Rectangle) innerBox.getShape();
+        int newX = l.getX() + innerX;
+        int newY = l.getY() + innerY;
+        return new Location(newX,newY,rect);
     }
 
     @Override
     public Location onRectangle(final Rectangle r) {
-        return null;
+        return new Location(0,0,r);
     }
 
     @Override
